2f66d917fef5bd7e0f3170eab8660f607a0994c5
Jared <jareddangelorusso@gmail.com>
2025-07-12 18:33:52 -0500

    se agregan los demas atributos al main.

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -2,20 +2,42 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User
 
+
 class UserAdmin(BaseUserAdmin):
     ordering = ['id']
-    list_display = ['email', 'is_staff', 'is_active']
-    search_fields = ['email']
+    list_display = ['email', 'nombre', 'nivel', 'puntos', 'is_staff', 'is_active']
+    search_fields = ['email', 'nombre', 'telefono']
+    
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
-        ('Permisos', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
-        ('Fechas', {'fields': ('last_login',)}),
+        ('Información personal', {
+            'fields': ('nombre', 'telefono', 'fecha_nacimiento')
+        }),
+        ('Fidelización', {
+            'fields': ('nivel', 'puntos')
+        }),
+        ('Permisos', {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'rol', 'groups', 'user_permissions')
+        }),
+        ('Fechas', {'fields': ('last_login', 'creado_en')}),
     )
+
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active')}
-        ),
+            'fields': (
+                'email',
+                'nombre',
+                'telefono',
+                'fecha_nacimiento',
+                'password1',
+                'password2',
+                'is_staff',
+                'is_active',
+                'rol',
+            )
+        }),
     )
+
 
 admin.site.register(User, UserAdmin)
